476616cc13ced995b511bc81990c61d66b05f4dd
Jesus Gastanaduy <47698202+j-gastanaduy@users.noreply.github.com>
2020-05-07 14:09:13 -0500

    R code

---
+++ HR_Attrition&Performance.R
@@ -0,0 +1,13 @@
+
+##libraries
+if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
+if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
+if(!require(data.table)) install.packages("data.table", repos = "http://cran.us.r-project.org")
+
+
+# IBM HR Analytics Employee Attrition & Performance Dataset
+#
+
+dl <- tempfile()
+download.file("", dl)
+
